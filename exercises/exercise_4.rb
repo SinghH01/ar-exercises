36ae51866ede68b7654d1cc1c8db75f9fb4f530a
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-03 16:33:06 -0600

    Create new stores and fetch data using where clause

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts "Total stores : #{Store.count}"
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "Total mens stores : #{@mens_stores.count}"
+
+@mens_stores.each {|store| 
+  puts "Name: #{store.name} | Annual Revenue: #{store.annual_revenue}"
+}
+
+@women_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000) 
+
+puts "Total women stores : #{@women_stores.count}"
