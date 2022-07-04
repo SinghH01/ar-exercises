cb020a77d010b2542dfa70e4f64b855c5c89470d
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-03 18:51:49 -0600

    Add multiple employees under different stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Harshbir", last_name: "Singh", hourly_rate: 100)
+@store1.employees.create(first_name: "Alex", last_name: "Thompson", hourly_rate: 70)
+@store2.employees.create(first_name: "Karan", last_name: "Kumar", hourly_rate: 20)
